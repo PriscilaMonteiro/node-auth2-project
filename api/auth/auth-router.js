0efc9ab052b9ad457ddd50f6f25294bb55595a1d
priscila monteiro <pririmonteiro@me.com>
2021-12-14 19:04:04 -0800

    mvp

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -61,4 +61,16 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
    */
 });
 
+function tokenBuilder(user){
+  const payload = {
+    subject: user.user_id,
+    role_name: user.role_name,
+    username: user.username
+  };
+  const options = {
+    expiresIn: '1d',
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+}
+
 module.exports = router;
