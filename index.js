47d0fddc545299727667aa6b4a738ed85d1f6337
priscila monteiro <pririmonteiro@me.com>
2021-12-14 17:30:12 -0800

    wip

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const { PORT } = require('./api/secrets/index');
+// const { PORT } = require('./api/secrets/index');
+const PORT = process.env.PORT || 9000;
+
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
